905c22ab0571b99f4af122fd70ca9e4103392340
Kishen Nagaraju <kishen@arproactive.com>
2024-11-26 19:18:58 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import path from "path"
 import react from "@vitejs/plugin-react"
+import path from "path"
 import { defineConfig } from "vite"
 
 export default defineConfig({
     plugins: [ react() ],
-    base: /shorty-url-shortener/,
+    base: "/shorty-url-shortener",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
